43633078d014daed99abcf016626666aa0dfcf95
konstantin <konstantin@juvigo.de>
2024-06-28 15:33:04 +0200

    Add generic bit string mutation

--- src/mutation.jl
+++ src/mutation.jl
@@ -1,2 +1,26 @@
 #ToDO
 # https://en.wikipedia.org/wiki/Mutation_(genetic_algorithm)
+
+
+"""
+    bit_string_mutation(gene)
+"""
+function bit_string_mutation(gene)
+    prob = 1/length(gene)
+    for i in range(1,length(gene))
+        if rand() < prob
+            gene[i] = !gene[i]
+        end
+    end
+end
+
+"""
+    bit_string_mutation(gene, prob)
+"""
+function bit_string_mutation(gene, prob)
+    for i in range(1,length(gene))
+        if rand() < prob
+            gene[i] = !gene[i]
+        end
+    end
+end
