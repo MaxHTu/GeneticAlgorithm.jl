790ea20f0892e905f576c28a29f729de426b84c5
konstantin <konstantin@juvigo.de>
2024-07-10 14:38:09 +0200

    Add todos

--- src/initiation.jl
+++ src/initiation.jl
@@ -1,3 +1,4 @@
+# TODO: Umbenennen dass die gleich heißen und die typen für den funktionsaufruf entscheidend sind
 """
     binary_initial_state(unitLen::Int, popSize::Int)
 
@@ -11,6 +12,7 @@ Initialize the population of popSize units. Each unit is a vector of unitLen Boo
 - Vector of units with random Boolean values.
 
 """
+# TODO: Size
 function binary_initial_state(unitLen::Int, popSize::Int)
     return [rand(Bool, unitLen) for _ in 1:popSize]
 end
@@ -29,6 +31,7 @@ Initialize the population of popSize units. Each unit is a vector of the same fo
 - Vector of units with random Float64 values in certain range.
 
 """
+# TODO: initValue oder genome size, mehrdimensional, range als vector von zwei werten + unit range für int oder float range für float
 function float_initial_state(popSize::Int, initValue::Vector{Float64}, range::UnitRange{Float64})
     # make sure value is always between range
     return [clamp.(initValue + rand(range,length(initValue)), range[1], range[end]) for _ in 1:popSize]
