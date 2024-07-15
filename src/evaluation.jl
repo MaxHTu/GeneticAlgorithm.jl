cdc0073fb1166d7dda2baffa35f631c89d19dc3b
MaxHTu <m.hiort@tu-berlin.de>
2024-07-15 12:24:26 +0200

    picture + evaluation docs

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -1,9 +1,4 @@
 """
-    This file contains a collection of common optimization test functions implemented in Julia to test the performance of the Genetic Algorithm. The functions are based on the paper [ON BENCHMARKING FUNCTIONS FOR GENETIC ALGORITHMS](https://web.archive.org/web/20190302201555id_/http://pdfs.semanticscholar.org/a8fe/54b39bd06fd703520bbd134c08a87e0baff0.pdf).
-FOR GENETIC ALGORITHMS
-"""
-
-"""
     sphere(x)
 
 Compute the sphere function value for a given input vector `x`.
