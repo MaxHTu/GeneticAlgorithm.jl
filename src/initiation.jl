3728428a2446a9386f3791067faff8d560e93f7b
Kirill Simonov <kirill-s@hotmail.com>
2024-07-16 23:45:26 +0200

    Fix initPop doc

--- src/initiation.jl
+++ src/initiation.jl
@@ -3,15 +3,15 @@
 
 This function initializes a population of individuals for a genetic algorithm.
 
-**Arguments**
+# Arguments
 - `popSize::Integer`: The number of individuals in the population.
 - `unitShape::AbstractVector{<:Integer}`: The shape of each individual in the population.
 - `unitValues::Union{Type,Vector{Bool},AbstractVector{<:Real},AbstractRange{<:Real}}`: The range of values for each element in the individual.
 
-**Returns**
+# Returns
 - `pop::Vector{<:AbstractArray}`: The initialized population.
 
-**Details**
+# Details
 - If `unitShape` is a scalar or a vector, the function generates random individuals with values from `unitValues`.
 - If `unitShape` is a matrix, the function generates random individuals with values from `unitValues`.
 """
